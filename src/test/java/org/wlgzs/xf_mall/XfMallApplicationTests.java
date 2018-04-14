12fd581788dbecc128a51b1c28fdf1ff82ccd496
FSJAlger <fsj.wlgzs@gmail.com>
2018-04-14 11:30:34 +0800

    first

---
+++ src/test/java/org/wlgzs/xf_mall/XfMallApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wlgzs.xf_mall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XfMallApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
