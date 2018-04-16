ba64e24032a8d2c6a27d079c27a95de74986565c
HuYaXing <911090257@qq.com>
2018-04-16 08:21:45 +0800

    Remove the conflict

--- src/main/java/org/wlgzs/xf_mall/entity/Orders.java
+++ src/main/java/org/wlgzs/xf_mall/entity/Orders.java
@@ -1,12 +1,8 @@
 package org.wlgzs.xf_mall.entity;
 
 import lombok.Data;
-
 import javax.persistence.*;
-<<<<<<< HEAD
-=======
 import java.util.Date;
->>>>>>> b77616cf504c907cb9edd662d58a1ad86e2e484c
 
 /**
  * @author:胡亚星
@@ -15,84 +11,7 @@ import java.util.Date;
  **/
 @Entity
 @Data
-<<<<<<< HEAD
 @Table(name = "xfm_order")
-public class Orders {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long order_id;//订单id
-
-    @Column(nullable = false)
-    private String product_picture;//商品图片
-
-    @Column(nullable = false,length = 30)
-    private String product_keywords;//商品关键字
-
-    @Column(nullable = false,length = 10)
-    private String product_counterPrice;//专柜价格
-
-    @Column(nullable = false,length = 10)
-    private String product_mallPrice;//商城价格
-
-    @Column(nullable = false)
-    private String product_specification;//商品规格信息
-
-    @Column(nullable = false,length = 10)
-    private String order_freight;//运费
-
-    @Column(nullable = false)
-    private Integer order_discount;//折扣
-
-    @Column(nullable = false)
-    private Integer product_isRedeemable;//是否可用积分兑换
-
-    @Column(nullable = false,length = 50)
-    private String order_number;//订单号
-
-    @Column(nullable = false,length = 10)
-    private String order_expressNumber;//订单快递号
-
-    @Column(nullable = false,length = 10)
-    private String order_status;//状态
-
-    @Column(nullable = false,length = 10)
-    private String order_receiver;//收货人
-
-    @Column(nullable = false,length = 10)
-    private String order_deliveryAddress;//发货地址
-
-    @Column(nullable = false,length = 10)
-    private String order_shippingAddress;//收货地址
-
-    @Column(nullable = false,length = 10)
-    private String order_phone;//收货地址
-
-    @Column(nullable = false,length = 10)
-    private String order_creationTime;//创建时间
-
-    @Column(nullable = false,length = 10)
-    private String order_purchaseTime;//下单时间
-
-    @Column(nullable = false,length = 10)
-    private String order_deliveryTime;//发货时间
-
-    @Column(nullable = false,length = 10)
-    private String order_dealTime;//成交时间
-
-    @Column(nullable = false,length = 10)
-    private String order_methodOfPurchase;//购买方式
-
-    @Column(nullable = false)
-    private Long product_id;//商品id
-
-    @Column(nullable = false)
-    private Long user_id;//用户id
-
-    @Column(nullable = false)
-    private Integer order_quantity;//购买数量
-
-}
-=======
 public class Orders {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -141,7 +60,5 @@ public class Orders {
     private String order_methodOfPurchase;//购买方式
     @Column(nullable = false)
     private int order_quantity;//购买数量
-
 }
 
->>>>>>> b77616cf504c907cb9edd662d58a1ad86e2e484c
--- src/main/java/org/wlgzs/xf_mall/entity/Product.java
+++ src/main/java/org/wlgzs/xf_mall/entity/Product.java
@@ -9,63 +9,7 @@ import javax.persistence.*;
  * @createTime 2018-04-14 16:43
  * @description:商品表
  **/
-<<<<<<< HEAD
-
-@Entity
-@Data
 @Table(name = "xfm_product")
-public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long product_id;//商品id
-
-    @Column(nullable = false,length = 10)
-    private String product_isShelf;//是否上架
-
-    @Column(nullable = false,length = 30)
-    private String product_keywords;//商品关键字
-
-    @Column(nullable = false,length = 10)
-    private String product_counterPrice;//专柜价格
-
-    @Column(nullable = false,length = 10)
-    private String product_mallPrice;//商城价格
-
-    @Column(nullable = false)
-    private String product_specification;//商品规格信息
-
-    @Column(nullable = false)
-    private String product_picture;//商品图片
-
-    @Column(nullable = false)
-    private String product_details;//商品详情
-
-    @Column(nullable = false,length = 100)
-    private String product_serviceType;//服务类型
-
-    @Column(nullable = false,length = 20)
-    private String product_category;//商品类别
-
-    @Column(nullable = false,length = 10)
-    private Integer product_isTop;//是否热门
-
-    @Column(nullable = false)
-    private String product_activity;//活动状态
-
-    @Column(nullable = false)
-    private Integer product_isRedeemable;//是否可用积分兑换
-
-    @Column(nullable = false,length = 20)
-    private String product_needsPoints;//需要多少积分
-
-    @Column(nullable = false,length = 20)
-    private String product_getCredits;//购买可获得多少积分
-
-    @Column(nullable = false,length = 10)
-    private Integer product_Inventory;//库存
-}
-=======
 @Entity
 @Data
 public class Product {
@@ -104,4 +48,3 @@ public class Product {
     private int product_Inventory;//库存
 }
 
->>>>>>> b77616cf504c907cb9edd662d58a1ad86e2e484c
